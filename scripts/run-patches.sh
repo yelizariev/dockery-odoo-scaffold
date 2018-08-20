fded264b7cd649d47d1d32ed82b3bfdfb77dd12b
David Arnold <dar@xoe.solutions>
2018-08-19 21:15:15 -0500

    [IMP] run-patches script with whoami

--- scripts/run-patches.sh
+++ scripts/run-patches.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-docker run --user=1000 -v $(pwd)/vendor/odoo/cc/odoo:/opt/odoo/odoo "${FROM}:${ODOO_VERSION}" apply-patches
+docker run --user=$(id -u $(whoami)) -v $(pwd)/vendor/odoo/cc/odoo:/opt/odoo/odoo "${FROM}:${ODOO_VERSION}" apply-patches
