7fd582178313779bdbf9199b5c6d60e1a4e7f533
David Arnold <dar@xoe.solutions>
2018-08-21 17:01:44 -0500

    [IMP] rebase command: catch failure and continue

--- scripts/run-dev-02-rebase.sh
+++ scripts/run-dev-02-rebase.sh
@@ -14,6 +14,28 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+
+rebase_alert() {
+    echo -e "\n${RED}The rebase failed (see logs).${NC}"
+    echo -e "${RED}Please open another terminal and handle the issue.${NC}"
+    read -p "Do you want to continue this script? (Y/n)" -n 1 -r
+    echo -e ""
+    if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+        eval "${gitcmd} rebase --abort"
+        eval "${gitcmd} checkout ${HEAD}"
+        eval "${gitcmd} branch -D ${newbranch}"
+        exit 1
+    fi
+    rebase_continue
+}
+
+rebase_continue () {
+    if ! eval "${gitcmd} rebase --continue"; then
+        rebase_alert
+    fi
+}
+
+
 for folder in "${folders[@]}"; do
     echo -e "\n\n${RED}Rebasing ${folder}${NC}\n\n"
     gitcmd="git --git-dir ${folder}"
@@ -35,12 +57,12 @@ for folder in "${folders[@]}"; do
 
                     branch=${candidate#"$owndev/"}
                     if eval "${gitcmd} checkout -b ${branch} ${candidate}"; then
-                        if eval "${gitcmd} rebase ${base}"; then
+                        if ! eval "${gitcmd} rebase ${base}"; then
+                            rebase_alert
+                        else
                             eval "${gitcmd} push -f ${owndev}"
                             eval "${gitcmd} checkout ${base}"
                             eval "${gitcmd} branch -D ${branch}"
-                        else
-                            exit 1
                         fi
                     else
                         echo -e "  Could not checkout ${candidate}\n"
