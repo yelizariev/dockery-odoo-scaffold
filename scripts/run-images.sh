6fff4f31a860fd793b523f5af871cf8f5cb376c0
David Arnold <dar@devco.co>
2018-08-31 15:32:05 -0500

    [FIX] pull -> --pull

--- scripts/run-images.sh
+++ scripts/run-images.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 source "${DIR}/../.env"
 
-if [ "$1" = 'pull' ]; then
+if [ "$1" = '--pull' ]; then
     docker pull "${FROM}:${ODOO_VERSION}"
 fi
 docker-compose -f docker-compose.yml build odoo
