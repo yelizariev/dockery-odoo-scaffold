ee7a8e55aeb445c5b2805b2cb027e84bb3c08fb2
David Arnold <dar@xoe.solutions>
2018-09-18 17:43:32 -0500

    [IMP] be more verbose during image build

--- scripts/run-images.sh
+++ scripts/run-images.sh
@@ -3,6 +3,10 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 source "${DIR}/../.env"
 
 if [ "$1" = '--pull' ]; then
@@ -16,6 +20,15 @@ if [ ! -d "/vendor/odoo/ee" ]; then
 	touch "${DIR}/../vendor/odoo/ee/.gitkeep"
 fi
 
-
+echo -e "\n${RED}First we build the production image. It contains:${NC}\n"
+echo -e "\t${GREEN}- Odoo Community Code${NC}\n"
+echo -e "\t${GREEN}- Odoo Enterprise Code (if configured)${NC}\n"
+echo -e "\t${GREEN}- Your customizations done in Dockerfile${NC}\n"
 docker-compose -f docker-compose.yml build odoo
+
+echo -e "\n${RED}Now we build the development image atop the production image.\n"
+echo -e "\t${GREEN}- Complements the image with additional tools used during local development${NC}\n"
+echo -e "\t${GREEN}- It uses a remote git context maintained by XOE.${NC}\n"
+echo -e "\t${GREEN}- Therefore, as shared knowledge about dev images evolves, just rebuild with \`docker-compose build odoo\`.${NC}\n"
+echo -e "\t${GREEN}- For details check: https://github.com/xoe-labs/dockery-odoo/blob/master/images/dev/Dockerfile${NC}\n"
 docker-compose build odoo
