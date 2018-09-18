7a6b1e5007b9f8e6d1e1a78646805977e197252c
David Arnold <dar@xoe.solutions>
2018-09-18 17:24:11 -0500

    [FIX] workflow with no enteprise clone

--- scripts/run-images.sh
+++ scripts/run-images.sh
@@ -8,5 +8,14 @@ source "${DIR}/../.env"
 if [ "$1" = '--pull' ]; then
     docker pull "${FROM}:${ODOO_VERSION}"
 fi
+
+# Create a dommy folder in case enterrpise has nt been cloned previously
+# Otherwise, docker build would fail (ONBUILD)
+if [ ! -d "/vendor/odoo/ee" ]; then
+	mkdir "${DIR}/../vendor/odoo/ee"
+	touch "${DIR}/../vendor/odoo/ee/.gitkeep"
+fi
+
+
 docker-compose -f docker-compose.yml build odoo
 docker-compose build odoo
