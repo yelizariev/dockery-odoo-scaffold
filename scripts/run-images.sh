9620e8dd6e8f2b9cd8aa1a651004819666db58f2
David Arnold <dar@xoe.solutions>
2018-09-19 11:55:39 -0500

    [IMP] run-image instructions

--- scripts/run-images.sh
+++ scripts/run-images.sh
@@ -21,14 +21,24 @@ if [ ! -d "/vendor/odoo/ee" ]; then
 fi
 
 echo -e "\n${RED}First we build the production image. It contains:${NC}\n"
-echo -e "\t${GREEN}- Odoo Community Code${NC}\n"
-echo -e "\t${GREEN}- Odoo Enterprise Code (if configured)${NC}\n"
-echo -e "\t${GREEN}- Your customizations done in Dockerfile${NC}\n"
+echo -e "\t${GREEN}- Odoo Community Code${NC}"
+echo -e "\t${GREEN}- Odoo Enterprise Code (if configured)${NC}"
+echo -e "\t${GREEN}- Vendored modules${NC}"
+echo -e "\t${GREEN}- Your project modules (\`src\` folder)${NC}"
+echo -e "\t${GREEN}- Your further customizations from the Dockerfile${NC}\n"
 docker-compose -f docker-compose.yml build odoo
 
 echo -e "\n${RED}Now we build the development image atop the production image.\n"
-echo -e "\t${GREEN}- Complements the image with additional tools used during local development${NC}\n"
-echo -e "\t${GREEN}- It uses a remote git context maintained by XOE.${NC}\n"
-echo -e "\t${GREEN}- Therefore, as shared knowledge about dev images evolves, just rebuild with \`docker-compose build odoo\`.${NC}\n"
-echo -e "\t${GREEN}- For details check: https://github.com/xoe-labs/dockery-odoo/blob/master/images/dev/Dockerfile${NC}\n"
+echo -e "\t${GREEN}- Complements additional tools for local dev${NC}"
+echo -e "\t${GREEN}- Remote build context maintained by XOE.${NC}"
+echo -e "\t${GREEN}- Therefore, as dev image evolves, just rebuild.${NC}"
+echo -e "\t${GREEN}- For details, visit: https://git.io/fA9xc${NC}\n"
 docker-compose build odoo
+
+echo -e "\n${RED}First time? Before you start, remeber to:${NC}\n"
+echo -e "\t${GREEN}- vendor your module dependencies and adapt the Dockerfile to load them;${NC}"
+echo -e "\t${GREEN}- apply patches to your local workdir: \`.scripts/run-patches.sh\`;${NC}"
+echo -e "\t\t${GREEN}ATTENTION: Never commit patches: stash them away, when tempted.${NC}"
+echo -e "\t\t${GREEN}They are already applied in the prod image.${NC}"
+echo -e "\t${GREEN}- scaffold a module or place one into the src folder; ${NC}"
+echo -e "\t${GREEN}- initialize Database by first \`docker-compose up odoo\`.${NC}\n"
