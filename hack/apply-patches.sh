f203f58112834e57eb07d6f27173be7f3feec4df
David Arnold <dar@xoe.solutions>
2018-10-29 16:04:32 -0500

    [IMP] UX of patch apply

--- hack/apply-patches.sh
+++ hack/apply-patches.sh
@@ -11,7 +11,7 @@ APPLY_DIR="${DIR}/../vendor/odoo/cc"
 source <(docker run --entrypoint= "${FROM}:${ODOO_VERSION}" cat /patches)
 PREV_PWD=$(pwd)
 cd "${DIR}/../vendor/odoo/cc/"
-git stash push --keep-index --include-untracked --message "Patches for ${ODOO_VERSION}"
-git stash apply stash@{0}
-echo -e "Patches stashed as: 'Patches for ${ODOO_VERSION}'"
+git stash push --keep-index --include-untracked --message "Patches for ${ODOO_VERSION}" &> /dev/null
+git stash apply stash@{0} &> /dev/null
+echo -e "${RED}Patches are in your submodule workdir (vendor/odoo/cc) and stashed there as: 'Patches for ${ODOO_VERSION}'${NC}"
 cd ${PREV_PWD}
