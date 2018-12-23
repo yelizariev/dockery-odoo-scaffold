6a340daab1c0f994dcb3e92866d03228494a85a3
David Arnold <dar@devco.co>
2018-12-23 00:56:07 -0500

    [FIX] apply patches on windows

--- hack/apply-patches.sh
+++ hack/apply-patches.sh
@@ -8,7 +8,7 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 APPLY_DIR="${DIR}/../vendor/odoo/cc"
-source <(docker run --entrypoint= "${FROM}:${ODOO_VERSION}" cat /patches)
+source <(docker run --entrypoint= "${FROM}:${ODOO_VERSION}-base" cat patches)
 PREV_PWD=$(pwd)
 cd "${DIR}/../vendor/odoo/cc/"
 git stash push --keep-index --include-untracked --message "Patches for ${ODOO_VERSION}" &> /dev/null
